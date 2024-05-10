7b13f365ef61929d5a58051db5af98964e7c65a4
Pushkar Sharma <96145925+Push4ck@users.noreply.github.com>
2024-05-10 17:27:55 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "320px",
+      sm: "375px",
+      md: "425px",
+      lg: "768px",
+      xl: "1024px",
+      xxl: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
